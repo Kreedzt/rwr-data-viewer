04dd165647a90fb5c883c71568b6a60732fba502
Kreedzt <zhaozisong1@live.com>
2022-08-03 21:09:40 +0800

    :sparkles: Kreedzt - add base button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <h1>RWR Data Viewer</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Incoming...
-        </p>
-      </div>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>RWR Data Viewer</h1>
+             <div className="card">
+                <Button
+                    variant="contained"
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    count is {count}
+                </Button>
+                <p>Incoming...</p>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
